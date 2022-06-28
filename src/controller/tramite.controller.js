50ee9078634f6923d02d0819a423a3c9c8bba2a9
Sergio <sergio.villanueva.rios@est.una.ac.cr>
2022-06-28 02:17:10 -0600

    TramiteCompleto

--- src/controller/tramite.controller.js
+++ src/controller/tramite.controller.js
@@ -9,7 +9,7 @@ export const postTramite = async (req, res) => {
         const newTramite = new tramite({codigo_tramite, tipo_tramite});
 
         if(codigo_departamento){
-            const foundDepartamento = await departamento.find({nombre_departamento: {$in: codigo_departamento}})
+            const foundDepartamento = await departamento.find({codigo_departamento: {$in: codigo_departamento}})
             newTramite.codigo_departamento = foundDepartamento.map(departamento => departamento._id)
     }
         const tramiteSaved = await newTramite.save()
@@ -24,3 +24,50 @@ export const postTramite = async (req, res) => {
         ));
     }
 };
+
+export const getTramite = async (req, res) => {
+    try {
+        const tramites = await tramite.find();
+        res.json(new BaseResponse(
+            "Tramites",
+            "Retorna todas los tramites",
+            tramites));
+    } catch (error) {
+        res.status(400).json(new ResponseError(
+            "Error al retornar los tramites",
+            error.message
+        ));
+    }
+};
+
+export const updateTramiteById = async (req, res) => {
+    try{
+    const updatedTramite = await tramite.findByIdAndUpdate(req.params.tramiteId, req.body, {
+        new: true
+    });
+    res.status(200).json(new BaseResponse(
+        "Tramite",
+        "Tramite actualizado",
+        updatedTramite));
+    }catch (error){
+    res.status(400).json(new ResponseError(
+        "Error al actualizar el tramite",
+        error.message
+    ));
+    }
+};
+
+export const deleteTramiteById = async (req, res) => {
+    try {
+        const tramiteIdDelete = await departamento.findByIdAndDelete(req.params.tramiteId);
+        res.status(200).json(new BaseResponse(
+            "Tramite",
+            "Se elimino un departamento con exito",
+            tramiteIdDelete));
+    }catch (error) {
+        res.status(400).json(new ResponseError(
+            "Error al eliminar el tramite",
+            error.message
+        ));
+    }
+}
--- src/routes/tramite.routes.js
+++ src/routes/tramite.routes.js
@@ -4,5 +4,8 @@ import * as tramiteCtrl from '../controller/tramite.controller';
 const router = Router()
 
 router.post('/', tramiteCtrl.postTramite)
+router.get('/getTramite', tramiteCtrl.getTramite)
+router.put('/:tramiteId', tramiteCtrl.updateTramiteById)
+router.delete('/:tramiteId', tramiteCtrl.deleteTramiteById)
 
 export default router;
